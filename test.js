f2ee00e7f02b8540c42afdc34cdb0fc84b9b17c8
Mohammed Flaifel <msflaifel@gmail.com>
2020-01-29 11:18:58 +0200

    delete and search function with their test relates #14

--- test.js
+++ test.js
@@ -1,4 +1,9 @@
-const { addItem } = require("./script/logic");
+const {
+  addItem,
+  deleteItemById,
+  searchFunction,
+  editFunction
+} = require("./script/logic");
 
 let itemArray = [
   {
@@ -21,6 +26,7 @@ let itemArray = [
   }
 ];
 
+///---expected***////
 let expectedAdditem = [
   {
     id: 6,
@@ -47,6 +53,37 @@ let expectedAdditem = [
     price: "310$"
   }
 ];
+
+let expectedDeleteItemById = [
+  {
+    id: 6,
+    title: "t_shirt",
+    details: "any details",
+    price: "25$"
+  },
+  {
+    id: 9,
+    title: "hathee",
+    details: "any details",
+    price: "30$"
+  }
+];
+
+let expectedSearchArr = [
+  {
+    id: 7,
+    title: "hat",
+    details: "any details",
+    price: "30$"
+  },
+  {
+    id: 9,
+    title: "hathee",
+    details: "any details",
+    price: "30$"
+  }
+];
+//addItem
 describe("Testing addItem  return new array with new element", () => {
   test("check lenght of array", () => {
     expect(
@@ -68,5 +105,26 @@ describe("Testing addItem  return new array with new element", () => {
     expect(actual[actual.length - 1]).toEqual(
       expectedAdditem[expectedAdditem.length - 1]
     );
+  });
+});
+
+//deleteItemById
+describe("Testing deleteItemById  return new array with removed element", () => {
+  test("check lenght of array", () => {
+    expect(deleteItemById(itemArray, 7).length).toEqual(
+      expectedDeleteItemById.length
+    );
+  });
+  test("deleteItemById function Should return new array without deleted item", () => {
+    let actual = deleteItemById(itemArray, 7);
+    expect(actual).toEqual(expectedDeleteItemById);
+  });
+});
+
+//search function
+describe("Testing searchFunction return search result", () => {
+  test("search the array with string ('ha') to show product that contains this string", () => {
+    let actual = searchFunction(itemArray, "ha");
+    expect(actual).toEqual(expectedSearchArr);
   });
 });
